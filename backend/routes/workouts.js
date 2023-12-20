bc00abad868f39e396b783688ae88a16dd34eca7
kippytech <dkipyegon.dk7.dk@gmail.com>
2023-12-20 12:30:53 +0300

    push mern workout app to github

---
+++ backend/routes/workouts.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout } = require('../controllers/workoutController')
+const requireAuth = require('../middleware/requireAuth')
+
+const router = express.Router()
+
+//require auth for all routes
+router.use(requireAuth)
+
+//get all workouts
+router.get('/', getWorkouts)
+
+//get a single workout
+router.get('/:id', getWorkout)
+
+//post a new workout
+router.post('/', createWorkout)
+
+//delete a workout
+router.delete('/:id', deleteWorkout)
+
+//UPDATE a workout
+router.patch('/:id', updateWorkout)
+
+module.exports = router
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useEffect } from 'react'
+import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useAuthContext } from '../hooks/useAuthContext'
+
+function Home() {
+  const {workouts, dispatch} = useWorkoutsContext()
+  const { user } = useAuthContext()
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+        const response = await fetch('http://localhost:4000/api/workouts', {
+          headers: {
+            'Authorization': `Bearer ${user.token}`
+          }
+        })
+        const json = await response.json()
+
+        if (response.ok) {
+            dispatch({type: 'SET_WORKOUTS', payload: json})
+        }
+    }
+
+    if (user) {
+      fetchWorkouts()
+    }
+    
+  }, [dispatch, user])
+  
+  return (
+    <div className="home">
+        <div className="workouts">
+            {workouts && workouts.map((workout) => (
+                <WorkoutDetails key={workout._id} workout={workout} />
+            ))}
+        </div>
+        <WorkoutForm />
+    </div>
+  )
+}
+
+export default Home
